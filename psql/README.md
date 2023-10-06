5fccac978e53fed9bdd92c3977e1f50dc835844e
maxthemus <max.somerille62@gmail.com>
2023-10-07 11:46:02 +1300

    All create table commands have been added

--- psql/README.md
+++ psql/README.md
@@ -1,9 +1,18 @@
 # PSQL docker container
 
+Prerequisites:
+Pulling PSQL with TimeScale DB extension docker image
+```
+$ docker pull timescale/timescaledb
+```
 How to build container
-> docker build -t <image-name>
+```
+$ docker build -t <image-name>
+```
 
 How to run conatiner
-> docker run --name <container-name> -e POSTGRES_PASSWORD=password -d <image-name>
+```
+$ docker run --name <container-name> -e POSTGRES_PASSWORD=password -d <image-name>
+```
 
 
--- psql/init.sql
+++ psql/init.sql
@@ -4,7 +4,35 @@ CREATE DATABASE IF NOT EXISTS kumeudb;
 --Connecting to db
 \c kumeudb
 
--- Creating tables
-CREATE TABLE IF NOT EXISTS, table_name (
-    info
+-- Creating tables --
+-- Create Vineyard table
+CREATE TABLE IF NOT EXISTS vineyard(
+  vineyard_id INTEGER PRIMARY KEY,
+  vineyard_name TEXT NOT NULL,
+  vineyard_location TEXT NOT NULL,
+  vineyard_size FLOAT NOT NULL
 );
+
+-- Create Node table
+CREATE TABLE IF NOT EXISTS node(
+  node_id TEXT PRIMARY KEY,
+  vineyard_id INTEGER REFERENCES VINEYARD(vineyard_id),
+  node_name TEXT NOT NULL
+);
+
+-- Create Measurement table
+CREATE TABLE IF NOT EXISTS measurement(
+  measurement_id INTEGER PRIMARY KEY,
+  node_id TEXT NOT NULL REFERENCES NODE(node_id),
+  timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
+  temperature FLOAT,
+  humidity FLOAT,
+  dew_point FLOAT,
+  wind_speed FLOAT,
+  leaf_wetness FLOAT,
+  rainfall FLOAT
+);
+
+ALTER USER postgres WITH PASSWORD ‘postgres’;
+ALTER ROLE postgres SET client_encoding TO 'utf8';
+GRANT ALL PRIVILEGES ON DATABASE kumeudb TO postgres;
